843652fe9fb5cf9f1c6cf27069ff7787a9e07c19
HalfsInner <kajbrz@hotmail.com>
2020-04-25 15:17:12 +0200

    Reformat

--- source/led_blinking.vhd
+++ source/led_blinking.vhd
@@ -1,25 +1,26 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    03:12:32 04/19/2020 
--- Design Name: 
--- Module Name:    led_blinking - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:    03:12:32 04/19/2020
+-- Design Name:
+-- Module Name:    led_blinking - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
+
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+  use IEEE.STD_LOGIC_1164.ALL;
+  use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,39 +31,45 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity led_blinking is
-	Port (
-		Clk : in STD_LOGIC;
-		LED : out STD_LOGIC_VECTOR(7 DOWNTO 0);
-        SW1 : in STD_LOGIC;
-        SW2 : in STD_LOGIC
-	);
-end led_blinking;
+entity LED_BLINKING is
+  port (
+    CLK : in    std_logic;
+    LED : out   std_logic_vector(7 downto 0);
+    SW1 : in    std_logic;
+    SW2 : in    std_logic
+  );
+end entity LED_BLINKING;
 
-architecture Behavioral of led_blinking is
-	--constant COUNTER_MAX : integer := 5000000;
-	signal counter : unsigned(3 downto 0);
-	constant COUNTER_MAX : integer := 10;
-	signal blinking_raw : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
-	signal blinking_out : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+architecture BEHAVIORAL of LED_BLINKING is
+
+  --constant COUNTER_MAX : integer := 5000000;
+  signal counter      : unsigned(3 downto 0);
+  constant counter_max : integer := 10;
+  signal blinking_raw : std_logic_vector(7 downto 0) := "00000000";
+  signal blinking_out : std_logic_vector(7 downto 0) := "00000000";
+
 begin
-	process(Clk)
-	begin 
-		if rising_edge(Clk) then
-			if counter >= COUNTER_MAX then
-				counter <= counter xor counter;
-                blinking_raw <= not blinking_raw;
-			else
-				counter <= counter + 1;
-			end if;
-                        
-            blinking_out <= blinking_raw;
-            if (not SW1 = '1') and (not SW2 = '1') then
-                blinking_out <= blinking_out or b"00000001";
-            end if;
-		end if;
-	end process;
-    --LED <= blinking_out;
-    LED <=  X"0" & STD_LOGIC_VECTOR(counter);
-	
-end Behavioral;
+
+  process (Clk) is
+  begin
+
+    if (Clk'event and Clk = '1') then
+      if (counter >= counter_max) then
+        counter      <= counter xor counter;
+        blinking_raw <= not blinking_raw;
+      else
+        counter <= counter + 1;
+      end if;
+
+      blinking_out <= blinking_raw;
+      if (not SW1 = '1') and (not SW2 = '1') then
+        blinking_out <= blinking_out or b"00000001";
+      end if;
+    end if;
+
+  end process;
+
+  --LED <= blinking_out;
+  LED <= X"0" & STD_LOGIC_VECTOR(counter);
+
+end architecture BEHAVIORAL;
--- testbench/test_led_blinking.vhd
+++ testbench/test_led_blinking.vhd
@@ -1,128 +1,131 @@
 --------------------------------------------------------------------------------
--- Company: 
+-- Company:
 -- Engineer:
 --
 -- Create Date:   22:01:14 04/20/2020
--- Design Name:   
+-- Design Name:
 -- Module Name:   /home/halfsinner/FPGA_learn/test_led_blinking.vhd
 -- Project Name:  FPGA_learn
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
+-- Target Device:
+-- Tool versions:
+-- Description:
+--
 -- VHDL Test Bench Created by ISE for module: led_blinking
--- 
+--
 -- Dependencies:
--- 
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
 --
--- Notes: 
+-- Notes:
 -- This testbench has been automatically generated using types std_logic and
 -- std_logic_vector for the ports of the unit under test.  Xilinx recommends
 -- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
+-- to guarantee that the testbench will bind correctly to the post-implementation
 -- simulation model.
 --------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
- 
+
+library ieee;
+  use ieee.std_logic_1164.ALL;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
- 
-ENTITY test_led_blinking IS
-END test_led_blinking;
- 
-ARCHITECTURE behavior OF test_led_blinking IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT led_blinking
-    PORT(
-         Clk : IN  std_logic;
-         LED : OUT  std_logic_vector(7 downto 0);
-         SW1 : IN  std_logic;
-         SW2 : IN  std_logic
-        );
-    END COMPONENT;
-    
 
-   --Inputs
-   signal Clk : std_logic := '0';
-   signal SW1 : std_logic := '0';
-   signal SW2 : std_logic := '0';
+entity TEST_LED_BLINKING is
+end entity TEST_LED_BLINKING;
 
- 	--Outputs
-   signal LED : std_logic_vector(7 downto 0);
+architecture BEHAVIOR of TEST_LED_BLINKING is
 
-   -- Clock period definitions
-   constant Clk_period : time := 10 ns;
-   constant reset_time : time := 100 ns;
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: led_blinking PORT MAP (
-          Clk => Clk,
-          LED => LED,
-          SW1 => not SW1,
-          SW2 => not SW2
-        );
+  -- Component Declaration for the Unit Under Test (UUT)
 
-   -- Clock process definitions
-   Clk_process :process
-   begin
-		Clk <= '0';
-		wait for Clk_period/2;
-		Clk <= '1';
-		wait for Clk_period/2;
-   end process;
- 
+  component LED_BLINKING is
+    port (
+      CLK : in    std_logic;
+      LED : out   std_logic_vector(7 downto 0);
+      SW1 : in    std_logic;
+      SW2 : in    std_logic
+    );
+  end component;
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-      wait for reset_time;	
-      SW1 <= '0';
-      SW2 <= '0';
-      
-      wait for Clk_period * 10;	
-      SW1 <= '1';
-      SW2 <= '1';
-      
-      wait for Clk_period * 10;	
-      SW1 <= '0';
-      SW2 <= '0';
-      
-      wait for Clk_period * 10;	
-      SW1 <= '0';
-      SW2 <= '1';
-      
-      wait for Clk_period * 10;	
-      SW1 <= '0';
-      SW2 <= '0';
-      
-      wait for Clk_period * 10;	
-      SW1 <= '1';
-      SW2 <= '1';
+  --Inputs
+  signal clk : std_logic := '0';
+  signal sw1 : std_logic := '0';
+  signal sw2 : std_logic := '0';
 
-      wait for Clk_period * 10;	
-      SW1 <= '0';
-      SW2 <= '0';
-      
-      wait for Clk_period * 10;	
-      SW1 <= '1';
-      SW2 <= '0';
+  --Outputs
+  signal led : std_logic_vector(7 downto 0);
 
-      
-      wait for Clk_period * 10;	
-      SW1 <= '1';
-      SW2 <= '1';
-      
-      wait;
-   end process;
+  -- Clock period definitions
+  constant clk_period : time := 10 ns;
+  constant reset_time : time := 100 ns;
 
-END;
+begin
+
+  -- Instantiate the Unit Under Test (UUT)
+  UUT : LED_BLINKING
+    port map (
+      CLK => clk,
+      LED => led,
+      SW1 => not sw1,
+      SW2 => not sw2
+    );
+
+  -- Clock process definitions
+  CLK_PROCESS : process
+  begin
+
+    clk <= '0';
+    wait for clk_period / 2;
+    clk <= '1';
+    wait for clk_period / 2;
+
+  end process CLK_PROCESS;
+
+  -- Stimulus process
+  STIM_PROC : process
+  begin
+
+    -- hold reset state for 100 ns.
+    wait for reset_time;
+    sw1 <= '0';
+    sw2 <= '0';
+
+    wait for clk_period * 10;
+    sw1 <= '1';
+    sw2 <= '1';
+
+    wait for clk_period * 10;
+    sw1 <= '0';
+    sw2 <= '0';
+
+    wait for clk_period * 10;
+    sw1 <= '0';
+    sw2 <= '1';
+
+    wait for clk_period * 10;
+    sw1 <= '0';
+    sw2 <= '0';
+
+    wait for clk_period * 10;
+    sw1 <= '1';
+    sw2 <= '1';
+
+    wait for clk_period * 10;
+    sw1 <= '0';
+    sw2 <= '0';
+
+    wait for clk_period * 10;
+    sw1 <= '1';
+    sw2 <= '0';
+
+    wait for clk_period * 10;
+    sw1 <= '1';
+    sw2 <= '1';
+
+    wait;
+
+  end process STIM_PROC;
+
+end architecture BEHAVIOR;
